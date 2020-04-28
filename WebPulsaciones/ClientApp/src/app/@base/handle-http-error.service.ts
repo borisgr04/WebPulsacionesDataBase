6a9bfc05cba83a262a5a0d3fafdb66f4d8d5378e
Boris Arturo González Rivera <borisgr04@gmail.com>
2020-04-28 15:48:45 -0500

    Se confirma implementación  de validación declarativa en el servidor

--- WebPulsaciones/ClientApp/src/app/@base/handle-http-error.service.ts
+++ WebPulsaciones/ClientApp/src/app/@base/handle-http-error.service.ts
@@ -13,17 +13,46 @@ export class HandleHttpErrorService {
   public handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       
-      console.error(error);
+      if (error.status == "500") {
+        this.mostrarError500(error);
+      }
 
-      const messageBox = this.modalService.open(AlertModalComponent)
-      messageBox.componentInstance.title = "Error al hacer la Operacón";
-      messageBox.componentInstance.message = error.error;
+      if (error.status == "400") {
+        this.mostrarError400(error);
+      }
 
       return of(result as T);
     };
   }
   public log(message: string) {
     console.log(message);
+  }
+
+  private mostrarError500(error: any): void {
+    console.error(error);
+  }
+
+  private mostrarError400(error: any): void {
+    console.error(error);
+    let contadorValidaciones: number = 0;
+    let mensajeValidaciones: string =
+      `Señor(a) usuario(a), se han presentado algunos errores de validación, por favor revíselos y vuelva a realizar la operación.<br/><br/>`;
+
+    for (const prop in error.error.errors) {
+      contadorValidaciones++;
+      mensajeValidaciones += `<strong>${contadorValidaciones}. ${prop}:</strong>`;
+
+      error.error.errors[prop].forEach(element => {
+        mensajeValidaciones += `<br/> - ${element}`;
+      });
+
+      mensajeValidaciones += `<br/>`;
+    }
+
+    const modalRef = this.modalService.open(AlertModalComponent);
+    modalRef.componentInstance.title = 'Mensaje de Error';
+    modalRef.componentInstance.message = mensajeValidaciones;
+
   }
 
 }
--- WebPulsaciones/Models/PersonaModel.cs
+++ WebPulsaciones/Models/PersonaModel.cs
@@ -1,6 +1,7 @@
 ﻿using Entity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,18 @@ namespace WebPulsaciones.Models
 {
     public class PersonaInputModel
     {
+        [Required]
         public string Identificacion { get; set; }
+        [Required(ErrorMessage ="El Nombre es requerido")]
         public string Nombre { get; set; }
+        [Required]
+        [Range(0,99, ErrorMessage ="Especifique una edad entre 0 y 99")]
         public int Edad { get; set; }
+        [Required]
+        [SexValidation(ErrorMessage = "Especifique un sexo [M ó F]")]
         public string Sexo { get; set; }
     }
-
+    
     public class PersonaViewModel : PersonaInputModel
     {
         public PersonaViewModel()
@@ -29,5 +36,16 @@ namespace WebPulsaciones.Models
             Pulsacion = persona.Pulsacion;
         }
         public decimal Pulsacion { get; set; }
+    }
+
+    public class SexValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (Convert.ToString(value) == "M" || Convert.ToString(value) == "F")
+                return ValidationResult.Success;
+            else
+                return new ValidationResult(ErrorMessage);
+        }
     }
 }
