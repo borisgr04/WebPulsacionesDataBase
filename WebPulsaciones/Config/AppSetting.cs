7280a0b3618810d1fd4afbc721c083d712906877
Boris Gonzalez <bgonzalez@byasystems.com.co>
2021-12-11 12:18:05 -0500

    se confirma corrección de construcción de jwt

--- WebPulsaciones/Config/AppSetting.cs
+++ WebPulsaciones/Config/AppSetting.cs
@@ -3,5 +3,9 @@
     public class AppSetting
     {
         public string Secret { get; set; }
+        public string ValidIssuer { get; set; }
+        public string ValidAudience { get; set; }
+        public int ExpiryInMinutes { get; set; }
+        
     }
 }
--- WebPulsaciones/Services/JwtService.cs
+++ WebPulsaciones/Services/JwtService.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -10,33 +11,50 @@ using WebPulsaciones.Models;
 
 namespace WebPulsaciones
 {
-    public class JwtService 
+    public class JwtService
     {
         private readonly AppSetting _appSettings;
-        public JwtService(IOptions<AppSetting> appSettings)=> _appSettings = appSettings.Value;
+        public JwtService(IOptions<AppSetting> appSettings) => _appSettings = appSettings.Value;
         public LoginViewModel GenerateToken(User userLogIn)
         {
-            // return null if user not found
-            if (userLogIn == null) return null;
-            var userResponse = new LoginViewModel() { FirstName = userLogIn.FirstName, LastName = userLogIn.LastName, Username = userLogIn.UserName, Role=userLogIn.Role };
-            // authentication successful so generate jwt token
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, userLogIn.UserName.ToString()),
-                    new Claim(ClaimTypes.Email, userLogIn.Email.ToString()),
-                    new Claim(ClaimTypes.MobilePhone, userLogIn.MobilePhone.ToString()),
-                    new Claim(ClaimTypes.Role, userLogIn.Role)
-                }),
-                Expires = DateTime.UtcNow.AddDays(7),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            };
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            userResponse.Token = tokenHandler.WriteToken(token);
+            var userResponse = new LoginViewModel() { FirstName = userLogIn.FirstName, LastName = userLogIn.LastName, Username = userLogIn.UserName, Role = userLogIn.Role };
+            var signingCredentials = GetSigningCredentials();
+            var claims = GetClaims(userLogIn);
+            var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+            userResponse.Token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
             return userResponse;
+        }
+  
+        private List<Claim> GetClaims(User userLogIn)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, userLogIn.UserName.ToString()),
+                new Claim(ClaimTypes.Email, userLogIn.Email.ToString()),
+                new Claim(ClaimTypes.MobilePhone, userLogIn.MobilePhone.ToString()),
+                new Claim(ClaimTypes.Role, userLogIn.Role)
+            };
+
+            return claims;
+        }
+        
+        private SigningCredentials GetSigningCredentials()
+        {
+            var key = Encoding.UTF8.GetBytes(_appSettings.Secret);
+            var secret = new SymmetricSecurityKey(key);
+
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+        private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _appSettings.ValidIssuer,
+                audience: _appSettings.ValidAudience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_appSettings.ExpiryInMinutes),
+                signingCredentials: signingCredentials);
+
+            return tokenOptions;
         }
     }
 }
